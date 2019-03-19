531d0632966cbb2bc7081af5181c0fc48b9381de
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@gmail.com>
2019-03-19 10:05:26 -0300

    Initial configurations

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+SRC=$PWD
+mkdir -p ~/.local/share/gtksourceview-3.0/language-specs/
+cd ~/.local/share/gtksourceview-3.0/language-specs/
+ln -s $SRC/todotxt.lang
+mkdir -p ~/.local/share/gtksourceview-2.0/language-specs/
+cd ~/.local/share/gtksourceview-2.0/language-specs/
+ln -s $SRC/todotxt.lang
